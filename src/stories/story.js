d2820b8efeba745e1b93d49578e15ec3f2007322
Harsh <mjkgoa@gmail.com>
2020-07-25 12:17:32 +0530

    fix: complete the story API

--- src/stories/story.js
+++ src/stories/story.js
@@ -1,4 +1,4 @@
-const ask = require('..util/ask');
+const ask = require('../util/ask');
 const { MessageEmbed } = require('discord.js');
 
 // NOTE: -1 in a story is the end of the story
@@ -45,13 +45,85 @@ function selectStory(
  * @param {Story} story 
  */
 function startStory(
-  client,
   channel,
   player,
   story
-) {
+) { 
   channel.send(`Starting story ${story.name}.`);
   channel.send(story.description);
+
+  startSituation(client, channel, player, story, 0);
+}
+
+/**
+ * 
+ * @param {DiscordClient} client
+ * @param {Channel} channel 
+ * @param {User} player 
+ * @param {Story} story 
+ * @param {number} situtationNo 
+ */
+function startSituation(
+  client,
+  channel,
+  player,
+  story,
+  situtationNo
+) {
+  const situation = story.situations[situtationNo];
+  channel.send(`\
+**${situation.title}**
+${situation.description}\
+`)
+
+  if (situation.nextQuestion === -1) channel.send(`**THE END**\n Life is short, isn't it?`);
+  else askQuestion(client, channel, player, story, situation.nextQuestion);
+}
+
+/**
+ * 
+ * @param {DiscordClient} client
+ * @param {Channel} channel 
+ * @param {User} player 
+ * @param {Story} story 
+ * @param {number} questionNo 
+ */
+function askQuestion(
+  client,
+  channel,
+  player,
+  story,
+  questionNo
+) {
+  let optionsString = '';
+  const question = story.questions[questionNo];
+
+  question.options.forEach((ques, i) => {
+    optionsString += `${i+1}) ${ques.opt}`;
+  })
+  
+  channel.send(optionsString);
+
+  const answerHandler = ans => {
+    const ansNo = Number(ans.trim().toLowerCase());
+
+    if (!isNaN(ansNo)) {
+      if (question.options.length <= ansNo) {
+        const nextSituationNo = question.options[ansNo - 1].nextSituation;
+        startSituation(client, channel, player, story, nextSituationNo);
+      }
+      else return ask(client, player, channel, `Choices in life are few, and the option you selected doesn't exist.`, answerHandler);
+    }
+    else return ask(client, player, channel, `I said number, fool!`, answerHandler);
+  }
+
+  ask(
+    client,
+    player,
+    channel,
+    `What do you want to do? (type option number)`,
+    answerHandler
+  )
 }
 
 module.exports = {
