d5d08027ed6c424632c53591c608cfb4d096d433
eniamza <eniamza0@gmail.com>
2020-08-12 00:16:49 +0600

    Feat : Added humor to Help command

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ client.on('message', msg => {
     selectStory(client, msg.channel, msg.author);
   }
   else if (msg.content.trim().toLowerCase() == `${prefix}help`) {
-    msg.channel.send(`For Bot support, bug reporting and Story submission Join https://discord.gg/4NZqsUs`); 
+    msg.channel.send(`Woah.. We thought it's easy as hell.Yet...\nFor Bot support, bug reporting and Story submission Join https://discord.gg/4NZqsUs`); 
   }
 })
   
