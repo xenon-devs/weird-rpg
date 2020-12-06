044cfb4c83316e59dd413e465509ab623e5a7afd
HarshKhandeparkar <mjkgoa@gmail.com>
2020-12-06 15:32:00 +0530

    fix: fix story-compiler again

--- story-compiler.sh
+++ story-compiler.sh
@@ -2,5 +2,5 @@ for storyFile in $(ls stories/*.jsonnet);
 do
   storyName=$(echo "$storyFile" | sed "s/\.jsonnet//")
 
-  ./jsonnet/jsonnet "stories/$storyName.jsonnet" -o "stories/$storyName.json"
+  ./jsonnet/jsonnet "$storyName.jsonnet" -o "$storyName.json"
 done
